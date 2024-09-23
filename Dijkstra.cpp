5520166130464c6499ef3547ddc5fd0dd6343bd7
Imposter <ara404.github@gmail.com>
2024-09-23 11:36:58 +0530

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -2,3 +2,15 @@
 #include<string>
 using namespace std;
 #define n 99
+
+class Matrix{
+	public :
+			int **arr;
+			int size=0;
+			int **weight;
+			int start;
+			string letter[6]={"A","B","C","D","E","F"};
+
+			void Values(int values[6][6]);
+			void Dijkstra();
+};
