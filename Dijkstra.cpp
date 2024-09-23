2e75d64639f7b97066db206761568435ac2f6585
Imposter <ara404.github@gmail.com>
2024-09-23 11:37:13 +0530

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -14,3 +14,19 @@ class Matrix{
 			void Values(int values[6][6]);
 			void Dijkstra();
 };
+
+void Matrix::Values(int values[6][6]){
+	arr=new int*[6];
+	weight=new int*[6];
+	int c=0;
+	for(int i=0;i<size;i++)
+	{
+		arr[i]=new int[6];
+		weight[i]=new int[2]; //weight[[sum,direction]]
+		for(int j=0;j<size;j++)
+		{
+			arr[i][j]=values[i][j];
+			weight[i][j]=n;
+		}
+	}
+}
