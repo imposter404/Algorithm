e18adada9696d6e54447fec42454692530e5a80c
Imposter <ara404.github@gmail.com>
2024-09-28 08:42:59 +0530

    Update Shift Cipher.cpp

--- Shift Cipher.cpp
+++ Shift Cipher.cpp
@@ -12,3 +12,15 @@ class String{
 			void Decrypt();
 };
 
+void String::Encrypt(){
+    for(int i=0;i<str.length();i++)
+    {
+        for(int
+		 j=0;j<26;j++)
+        {
+            if(x[j]==toupper(str[i])){
+                encrypt+=x[(j+key)%26];
+            }
+        }
+    }
+}
