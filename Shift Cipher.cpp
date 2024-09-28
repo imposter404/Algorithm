a259f7a01457ed30083674db6db7e666e7941106
Imposter <ara404.github@gmail.com>
2024-09-28 08:39:54 +0530

    Update Shift Cipher.cpp

--- Shift Cipher.cpp
+++ Shift Cipher.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
 #include<string>
+
+class String{
+	public :
+			string str;
+			string encrypt;
+			string decrypt;
+			int key;
+            string x="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			void Encrypt();
+			void Decrypt();
+};
