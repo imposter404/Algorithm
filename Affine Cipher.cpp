dbe5da054661a242bbc2b49204fa068bfe6a4b52
Imposter <ara404.github@gmail.com>
2024-09-28 08:46:04 +0530

    Update Affine Cipher.cpp

--- Affine Cipher.cpp
+++ Affine Cipher.cpp
@@ -15,3 +15,17 @@ class String{
 			void Encrypt();
 			 void Decrypt();
 };
+
+
+void String::Encrypt(){
+    for(int i=0;i<str.length();i++)
+    {
+        for(int
+		 j=0;j<26;j++)
+        {
+            if(l[j]==toupper(str[i])){
+                encrypt+=l[(a*j+b)%26];
+            }
+        }
+    }
+}
