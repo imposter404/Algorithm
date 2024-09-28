05ed26e980fa9e3810d064f6981d6da077a517c8
Imposter <ara404.github@gmail.com>
2024-09-28 08:45:49 +0530

    Update Affine Cipher.cpp

--- Affine Cipher.cpp
+++ Affine Cipher.cpp
@@ -2,3 +2,16 @@
 #include<string>
 #include<stack>
 using namespace std;
+
+
+class String{
+	public :
+			string str;
+			string encrypt;
+			string decrypt;
+			int a;
+			int b;
+            string l="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			void Encrypt();
+			 void Decrypt();
+};
