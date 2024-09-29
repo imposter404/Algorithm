831de59b2c9f3ad81bbe25cf006a380e86b12103
Imposter <ara404.github@gmail.com>
2024-09-29 10:45:11 +0530

    Update Longest Common Subsequence.cpp

--- Longest Common Subsequence.cpp
+++ Longest Common Subsequence.cpp
@@ -14,3 +14,21 @@ class Matrix{
 			void LCS();
 			void display();
 };
+
+void Matrix::values(){
+    matrix=new int*[x.length()+1];
+    for(int i=0;i<=x.length();i++)
+	{
+		matrix[i]=new int[y.length()+1];
+	}
+    for(int i=0;i<=x.length();i++)
+	{
+		for(int j=0;j<=y.length();j++)
+		{
+			matrix[i][j]=0;
+			matrix[i][j]=0;
+		}
+	}	
+}
+
+
