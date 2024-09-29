25f819a52d4d359bbb06052491367d091dfbaea7
Imposter <ara404.github@gmail.com>
2024-09-29 10:44:58 +0530

    Update Longest Common Subsequence.cpp

--- Longest Common Subsequence.cpp
+++ Longest Common Subsequence.cpp
@@ -2,3 +2,15 @@
 #include<string>
 #include<stack>
 using namespace std;
+
+class Matrix{
+	public :
+			int **matrix;
+			int size=0;
+			string x;
+			string y;
+            stack <int> sta;
+            void values();
+			void LCS();
+			void display();
+};
