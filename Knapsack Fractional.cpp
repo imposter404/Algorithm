cc421383c7f5a96f28bc35df034fd02b9dd33005
Imposter <ara404.github@gmail.com>
2024-09-29 10:50:41 +0530

    Update Knapsack Fractional.cpp

--- Knapsack Fractional.cpp
+++ Knapsack Fractional.cpp
@@ -1,3 +1,14 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+class Matrix{
+	public :
+			float **matrix;
+			int x_size;
+			int n;
+			int profit;
+            void Values(int values[6][3]);
+			void knapsack();
+			void display();
+};
