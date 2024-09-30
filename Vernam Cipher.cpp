0d65a753bc6c40413e3bd6ccd5994edbe9bdc3df
Imposter <ara404.github@gmail.com>
2024-09-30 15:40:41 +0530

    Update Vernam Cipher.cpp

--- Vernam Cipher.cpp
+++ Vernam Cipher.cpp
@@ -11,3 +11,12 @@ class String{
 		void Encrypt();
 		void Decrypt();
 };
+
+void String::Encrypt(){
+    for(int i=0;i<str.length();i++)
+    {
+        encrypt+=char(((int(toupper(str[i]))-int('A')) + (int(toupper(key[i%key.length()]))-int('A')))%26+int('A'));
+        // hell yeah i managed to do all the things in one line :)
+        // with O(n) :)
+    }
+}
