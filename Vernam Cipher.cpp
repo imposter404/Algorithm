7845b59b051f731b4e93830757b7e29e22c9f2c8
Imposter <ara404.github@gmail.com>
2024-09-30 15:39:47 +0530

    Update Vernam Cipher.cpp

--- Vernam Cipher.cpp
+++ Vernam Cipher.cpp
@@ -1,3 +1,13 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+class String{
+	public :
+		string str;
+		string encrypt;
+		string decrypt;
+		string key;
+		void Encrypt();
+		void Decrypt();
+};
